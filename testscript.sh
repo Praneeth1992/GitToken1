459970835471bfab639a045cfb3f9343f7198597
Jenkins Automation Server <jenkins@labu111c.mylabserver.com>
2019-04-30 20:27:26 +0000

    Added second line

--- testscript.sh
+++ testscript.sh
@@ -3,5 +3,6 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "I have added one line"
+echo "I have added second line"
 echo "this was run automatically first"
 
